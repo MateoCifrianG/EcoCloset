d1012c095937bc5b174f8acefc0418f8eab499e5
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-11-01 15:50:57 +0100

    Creo la ventana de registro

--- src/VentanaRegistro.java
+++ src/VentanaRegistro.java
@@ -1,8 +1,24 @@
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
 
 public class VentanaRegistro {
-
+	
+	private JFrame ventana;
+	
+	public VentanaRegistro() {
+		// CREACIÓN DE LA VENTANA DE REGISTRO
+        ventana = new JFrame("Registro");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(400, 400); // Aumentar el tamaño para el nuevo enlace
+        ventana.setLayout(new GridBagLayout()); //Para que todo mantenga su tamaño
+        
+      
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		
 
