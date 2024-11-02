20d855c5710c304503b13a9d1210f3ec22dee3a9
Jorge Cobián <j.cobian@opendeusto.es>
2024-11-02 17:11:48 +0100

    Cree el panel de filtros con los precios y tallas

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,9 +1,12 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VentanaPrincipal {
@@ -30,6 +33,37 @@ public class VentanaPrincipal {
 		
 		//Añadimos el panel de compra venta a la ventana arriba de todo (norte)
 		ventana.add(panelCompraVenta, BorderLayout.NORTH);
+		
+		//Creamos el panel princial
+		JPanel panelPrincipal = new JPanel(new BorderLayout());
+		
+		//creamos el panel superior
+		JPanel buscador = new JPanel(new GridLayout(2, 3, 10, 10));
+        buscador.setBackground(new Color(152, 251, 152)); // Azul clarito en rgb
+		JLabel vacio = new JLabel();
+		JButton filtrar = new JButton("FILTRAR");
+		JLabel vacio2 = new JLabel();
+		
+		// Cambiamos el botón de precio por un JComboBox de rangos de precios
+		String[] rangosPrecio = { "Precio", "0-15", "15-30", "30-50", "50-100", "100-200", "200-500", "500-1000", "1000-5000", "5000+" };
+		JComboBox<String> comboPrecio = new JComboBox<>(rangosPrecio);
+		comboPrecio.setFont(new Font("Courier", Font.BOLD, 15));
+			
+		// Cambiamos el JComboBox de número por uno de tallas
+		String[] tallas = { "Talla", "XS", "S", "M", "L", "XL", "XXL" };
+		JComboBox<String> comboTalla = new JComboBox<>(tallas);
+		comboTalla.setFont(new Font("Courier", Font.BOLD, 15));
+		
+		//Añadimos todo
+		buscador.add(vacio);
+		buscador.add(filtrar);
+		buscador.add(vacio2);
+		buscador.add(comboPrecio); // Añadir JComboBox de precios
+		buscador.add(comboTalla); 
+		//Falta la ropa, tenemos que crear la base de datos de todas las prendas para poder meterla.
+		
+		
+		ventana.setVisible(true);
 			
 	}
 	
