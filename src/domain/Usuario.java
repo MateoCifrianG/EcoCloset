a1bfbe263dd558ab52daab5932a1f71792fc754a
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-11-02 18:20:57 +0100

    Creación de la clase Usuario

---
+++ src/domain/Usuario.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Usuario {
+	
+	private String nombre;
+	
+	public Usuario ( String nombre) {  // Constructor
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+
+}
