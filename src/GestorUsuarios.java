418a50f8d4e93401585ea1ccd2885270e28bb442
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-11-03 18:01:08 +0100

    Creo la ventana del gestor de usuarios

--- src/GestorUsuarios.java
+++ src/GestorUsuarios.java
@@ -1,8 +1,33 @@
+import java.awt.BorderLayout;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class GestorUsuarios {
-
+	private JFrame ventana;
+    private JTable tablaUsuarios;
+    private DefaultTableModel modeloTabla;
+    private JTextField campoBusqueda;
+    private List<String[]> listaUsuarios;
+    
+	public GestorUsuarios() {
+		//Creo la ventana del gestor de usuarios
+		ventana = new JFrame("Gestor de Usuarios");
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setSize(800, 600); //Tamaño de la ventana
+		ventana.setLayout(new BorderLayout());
+		
+		
+		ventana.setVisible(true);
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new GestorUsuarios();
 
 	}
 
