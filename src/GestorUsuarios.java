398aabd15ce12deb1a675f1cedf62d1ae5c2252b
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-11-03 19:06:14 +0100

    Añado los usuarios a la tabla

--- src/GestorUsuarios.java
+++ src/GestorUsuarios.java
@@ -77,7 +77,7 @@ public class GestorUsuarios {
 	        } catch (IOException e) {
 	            JOptionPane.showMessageDialog(ventana, "Error al leer el archivo CSV: " + e.getMessage());
 	        }
-	        return usuarios; //devuelve la lista de usuarios
+	        return usuarios; //devuelve la lista de los usuarios
 		}
 		
 		//Método para actualizar la tabla con usuarios
