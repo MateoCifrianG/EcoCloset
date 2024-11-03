b69c52fe4ba8f6eefc760afe30f90b95eaa0b930
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-11-03 18:54:56 +0100

    Creación del método actualizarCantidadEnCSV en la clase bdRopa

--- src/bd/BaseDatosRopa.java
+++ src/bd/BaseDatosRopa.java
@@ -1,7 +1,9 @@
 package bd;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,11 @@ import domain.Ropa;
 
 public class BaseDatosRopa {
 
-	private static final String FILE_PATH = "productos.csv";
-
 	// Método para obtener la lista de ropa
 	public List<Ropa> getRopaList() {
 		List<Ropa> ropaList = new ArrayList<>();
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+		try (BufferedReader reader = new BufferedReader(new FileReader("productos.csv"))) {
 
 			String line;
 			while ((line = reader.readLine()) != null) {
@@ -49,4 +49,61 @@ public class BaseDatosRopa {
 		return ropaList;
 
 	}
+	
+	
+	// Método para actualizar la cantidad en el CSV
+	public void actualizarCantidadEnCSV (Ropa prenda) {
+		
+		List<String> lineas = new ArrayList<String>(); // lista para almacenar las lineas del nuevo archivo
+		boolean eliminado = false; // para verificar si se eliminó la prenda
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader("productos.csv"))) {
+			String linea;
+			while ((linea = reader.readLine()) != null) {
+				String[] valores = linea.split(";"); // csv separado por ";"
+				
+				// Verificar si la línea corresponde a la prenda que queremos actualizar
+				if (valores[0].equals(prenda.getNombre()) && valores[1].equals(prenda.getMarca()) && valores[2].equals(prenda.getTalla())) {
+					
+					int cantidadActual = Integer.parseInt(valores[3]);
+					cantidadActual --; // disminuir la cantidad
+					
+					// Comprobar si la cantidad llegó a cero
+					if (cantidadActual <= 0) {
+						eliminado = true; // marcamos que se ha eliminado la prenda
+						continue; // no añadimos esta linea al nuevo contenido
+					} else {
+						valores[3] = String.valueOf(cantidadActual); // actualizar la cantidad
+					}
+				}
+				// Añadir la línea al nuevo contenido si no fue eliminada
+				lineas.add(String.join(";", valores));
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error al leer el archivo CSV para actualizar la antidad.");
+		}
+		
+		// Escribir el nuevo contenido en el archivo CSV
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter("productos.csv"))) {
+			
+			for (String nuevaLinea : lineas) {
+				writer.write(nuevaLinea);
+				writer.newLine(); // nueva linea
+			}
+			
+			if (eliminado) {
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Error al leer el archivo CSV.");
+		}
+		
+	}
+	
+	
+	
+	
 }
