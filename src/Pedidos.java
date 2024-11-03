7878adbc20c0224cfc4fa94dfddc5d2598083e09
Jorge Cobián <j.cobian@opendeusto.es>
2024-11-03 19:26:19 +0100

    Cambie de localización de la clase Pedidos al principal y cree la ventana + el panel superior

---
+++ src/Pedidos.java
@@ -0,0 +1,26 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Pedidos {
+	private JFrame ventana;
+	
+	public Pedidos() {
+		
+		//Creamos la ventana 
+		ventana = new JFrame("Administrador de Pedidos");
+	    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    ventana.setSize(800, 600);
+	    ventana.setLayout(new BorderLayout());
+	    
+	    // Creamos el panel superior con campo de búsqueda y botón de eliminar
+	    JPanel panelSuperior = new JPanel(new BorderLayout());
+        JTextField campoBusqueda = new JTextField();
+        JButton botonBuscar = new JButton("Buscar");
+        JButton botonEliminar = new JButton("Eliminar");
+	}
+
+}
