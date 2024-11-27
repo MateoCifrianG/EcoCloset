7cd4ce90d8e416ce27c8f30dab060c227c16db0f
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-11-27 19:03:54 +0100

    Creo la ventana

--- src/Asistencia.java
+++ src/Asistencia.java
@@ -1,4 +1,17 @@
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
 
 public class Asistencia {
-
+	private JFrame ventanaProblemas;
+	
+	public Asistencia() {
+		//Creo la ventana de Problemas
+		ventanaProblemas = new JFrame("Problemas al iniciar sesión");
+		ventanaProblemas.setSize(400,400);
+		ventanaProblemas.setLayout(new GridLayout());
+		
+		
+		ventanaProblemas.setVisible(true);
+	}
 }
