7657e471fe25b938f3efdb829e58674288019548
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-11-27 19:15:30 +0100

    Creo los campos

--- src/Asistencia.java
+++ src/Asistencia.java
@@ -1,6 +1,10 @@
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Asistencia {
 	private JFrame ventanaProblemas;
@@ -11,6 +15,15 @@ public class Asistencia {
 		ventanaProblemas.setSize(400,400);
 		ventanaProblemas.setLayout(new GridLayout());
 		
+		JLabel usuarioLabel = new JLabel("Usuario");
+		JTextField usuarioField = new JTextField(20);
+		
+		JLabel correoLabel = new JLabel("Correo Electrónico:");
+		JTextField correoField = new JTextField(20); //Campo para el correo electrónico
+		
+		JLabel mensajeLabel = new JLabel("Comentario:");
+		JTextArea mensajeArea = new JTextArea(5, 20); //5 filas, 20 columnas
+		JButton enviarButton = new JButton("Enviar");
 		
 		ventanaProblemas.setVisible(true);
 		
