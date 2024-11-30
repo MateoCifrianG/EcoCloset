77bac75fa8778e997fd44bff833f2decb502c3c7
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-11-30 16:52:58 +0100

    Creo el panel superior de la VentanaAsistencia y añado sus componentes

--- src/VentanaAsistencia.java
+++ src/VentanaAsistencia.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class VentanaAsistencia {
@@ -10,11 +13,29 @@ public class VentanaAsistencia {
 	
 	public VentanaAsistencia() {
 		
-		//Ceación de la VentanaAsistencia
+		// Ceación de la VentanaAsistencia
 		ventana = new JFrame("Gestor de Asistencia");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ventana.setSize(800, 600); // Tamaño de la ventana
 		ventana.setLayout(new BorderLayout());
+		
+		// Panel superior con filtro de búsqueda y botón de eliminar
+		JPanel panelSuperior = new JPanel(new BorderLayout());
+		JPanel panelBusqueda = new JPanel(new BorderLayout());
+		
+		campoBusqueda = new JTextField();
+		JLabel buscarAsistencia = new JLabel("Buscar Asistencia: ");
+		JButton botonBuscar = new JButton("Buscar");
+		JButton botonEliminar = new JButton("Eliminar");
+		
+		panelBusqueda.add(buscarAsistencia, BorderLayout.WEST);
+		panelBusqueda.add(campoBusqueda, BorderLayout.CENTER);
+		panelBusqueda.add(botonBuscar, BorderLayout.EAST);
+		
+		panelSuperior.add(panelBusqueda, BorderLayout.NORTH);
+		panelSuperior.add(botonEliminar, BorderLayout.SOUTH);
+
+		
 		
 	}
 }
