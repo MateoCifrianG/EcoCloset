0587cc2b9b01d8ce9c55c7cce562e44859ac6f28
David Oficialdegui <david.oficialdegui@opendeusto.es>
2024-11-30 11:00:34 +0100

    Añado un Label de titulo y lo pongo bonito (VentanaCuenta)

--- src/VentanaCuenta.java
+++ src/VentanaCuenta.java
@@ -1,8 +1,11 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VentanaCuenta {
@@ -30,7 +33,11 @@ public class VentanaCuenta {
 		panelInformacion.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Espaciado interno
 		panelInformacion.setOpaque(false); // hacer el panel transparente
 		
-		
+		// Titulo
+		JLabel titulo = new JLabel("Bienvenido, " + nombreUsuario);
+		titulo.setFont(new Font("Arial", Font.BOLD, 28)); // cambiar fuente y tamaño
+		titulo.setForeground(new Color(70, 130, 180)); // Color del texto
+		panelInformacion.add(titulo);
 		
 		
 		
