27aba2860b4e90f2fcf087bbb917192b7558a0c2
Jorge Cobián <j.cobian@opendeusto.es>
2024-11-30 16:23:00 +0100

    Mostrar los pedidos del usuario

--- src/VentanaPedidos.java
+++ src/VentanaPedidos.java
@@ -1,6 +1,14 @@
 
 public class VentanaPedidos {
-
+	public VentanaPedidos(String nombreUsuario) {
+        // mostrar los pedidos de un usuario
+        JFrame ventana = new JFrame("Mis Pedidos");
+        ventana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        ventana.setSize(600, 400);
+        ventana.setLocationRelativeTo(null); // Centra la ventana
+        ventana.getContentPane().setBackground(new Color(240, 248, 255)); // Fondo azul clarito
+        
+	}
 	public static void main(String[] args) {
 		
 
