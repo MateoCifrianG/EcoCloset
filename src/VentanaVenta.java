9def2267584b20c25a91bd580ce0f707a6dcbfbc
Mateo Cifrian <mateo.cifrian@opendeusto.es>
2024-12-01 18:23:47 +0100

    creacion ventana Venta

--- src/VentanaVenta.java
+++ src/VentanaVenta.java
@@ -1,4 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class VentanaVenta {
+    private JFrame frame;
 
+    public VentanaVenta() {
+        frame = new JFrame("Añadir Ropa - Venta");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new GridLayout(7, 2));
+    }
 }
