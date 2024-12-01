0917bbf490b179bd1f5b8fe6c801bb3b0a50eafd
Mateo Cifrian <mateo.cifrian@opendeusto.es>
2024-12-01 23:05:56 +0100

    Merge branch 'main' of https://github.com/MateoCifrianG/EcoCloset into main

--- src/gui/BotonesPrincipal.java
+++ src/gui/BotonesPrincipal.java
@@ -2,7 +2,7 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-	
+ 	
 	public class BotonesPrincipal {
 	    private VentanaPrincipal ventanaPrincipal;
 	
