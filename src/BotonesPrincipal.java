d6a8d8e00c6f266c1ced88632355e8cf0f48846a
Mateo Cifrian <mateo.cifrian@opendeusto.es>
2024-12-01 18:18:23 +0100

    funcion opcion1

--- src/BotonesPrincipal.java
+++ src/BotonesPrincipal.java
@@ -1,4 +1,22 @@
-
-public class BotonesPrincipal {
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+	
+	public class BotonesPrincipal {
+	    private VentanaPrincipal ventanaPrincipal;
+	
+	    public BotonesPrincipal(VentanaPrincipal ventanaPrincipal) {
+	        this.ventanaPrincipal = ventanaPrincipal;
+	    }
+	
+	    // Método para obtener el ActionListener de Opción 1
+	    public ActionListener getAbrirCuenta() {
+	        return new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                ventanaPrincipal.getVentana().dispose(); // Cerrar la ventana actual usando el método getter
+	                ventanaPrincipal.abrirVentanaCuenta();
+	            }
+	        };
+	    }
 }
