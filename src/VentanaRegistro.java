86b55f216fa1285245e680a36eb7ce3566542508
Jorge Cobián <j.cobian@opendeusto.es>
2024-12-01 22:34:00 +0100

    admin arreglado

--- src/VentanaRegistro.java
+++ src/VentanaRegistro.java
@@ -89,16 +89,23 @@ public class VentanaRegistro {
         // Crear un hilo que simula la verificación
         Thread hilo = new Thread(() -> {
             try {
-                
-                if (Comprobador.verificarUsuario(nombre, contraseña)) { 
+                // Verificar si el usuario es el administrador
+                if (nombre.equals("admin") && contraseña.equals("admin")) {
+                    // Si es admin, se abre el menú de administrador
+                    SwingUtilities.invokeLater(() -> {
+                        mensaje.setText("¡Acceso administrador exitoso!");
+                        ventana.dispose(); // Cerrar la ventana actual
+                        new MenuAdministrador(); // Abrir ventana del administrador
+                    });
+                } else if (Comprobador.verificarUsuario(nombre, contraseña)) { 
+                    // Si no es admin pero las credenciales son correctas, se abre la ventana principal
                     for (int i = 0; i <= 100; i++) {
                         Thread.sleep(20); 
                         barraProgreso.setValue(i);
                     }
-                  
                     SwingUtilities.invokeLater(() -> {
                         mensaje.setText("¡Inicio de sesión exitoso!");
-                        abrirVentanaPrincipal(nombre); // Abrir ventana principall
+                        abrirVentanaPrincipal(nombre); // Abrir ventana principal
                     });
                 } else {
                     SwingUtilities.invokeLater(() -> {
@@ -113,6 +120,7 @@ public class VentanaRegistro {
         });
         hilo.start(); // Iniciar el hilo
     }
+
 //p
 
     // Método para abrir la ventana principal
