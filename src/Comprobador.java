4b29a860a36101945f3a0d045cb479751c09c076
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2024-12-01 21:56:08 +0100

    Hago el método obtenerDatosUsuarios con el sql

--- src/Comprobador.java
+++ src/Comprobador.java
@@ -29,6 +29,53 @@ public class Comprobador {
         }
         return null;  // Si no coincide, retorna null
     }
+    
+//    public static boolean obtenerDatosUsuarios(String nombreIngresado, String contraseñaIngresada) {
+//		
+//		List<Usuario> lUsuarios = new ArrayList<Usuario>();
+//		
+//		// cargar el driver de SQLite para JDBC
+//		// se hace una vez en todo el programa
+//		try { 
+//			Class.forName("org.sqlite.JDBC"); 
+//		} catch (ClassNotFoundException e) { 
+//			System.out.println("No se ha podido cargar el driver de la base de datos"); 
+//		}
+//		
+//		// conectar a la base de datos
+//		try {
+//			Connection conn = DriverManager.getConnection("jdbc:sqlite:resources/usuarios.sqlite");
+//			Statement stmt = conn.createStatement();
+//			
+//			String sql = "SELECT * FROM usuarios";
+//			ResultSet rs = stmt.executeQuery(sql);
+//			while (rs.next()) {
+//				  
+//				String nombreUsuario = rs.getString("Nombre");
+//				String contraseña = rs.getString("Contraseña");	
+//				
+//				Usuario usuario = new Usuario(nombreUsuario, contraseña);
+//				lUsuarios.add(usuario);
+//			}
+//							
+//		} catch (SQLException e1) {
+//			e1.printStackTrace();
+//		}
+//		
+//		// Comparar nombre de usuario y contraseña
+//		boolean comprobacion = false;
+//		
+//		for (Usuario u : lUsuarios) {
+//			if (u.getNombre().equals(nombreIngresado) && u.getContraseña().equals(contraseñaIngresada)) {
+//				comprobacion = true;
+//			} else {
+//				comprobacion = false;
+//			}
+//		}
+//		
+//		return comprobacion; 
+//		
+//}
 
     // Nuevo método para verificar si un usuario es válido
     public static boolean verificarUsuario(String nombreIngresado, String contraseñaIngresada) {
