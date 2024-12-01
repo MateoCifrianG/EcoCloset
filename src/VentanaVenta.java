43b787b2f137e124f51036449432246aeaa6c0b1
Mateo Cifrian <mateo.cifrian@opendeusto.es>
2024-12-01 18:27:05 +0100

    Crear un JComboBox para la cantidad

--- src/VentanaVenta.java
+++ src/VentanaVenta.java
@@ -25,6 +25,8 @@ public class VentanaVenta {
         JComboBox<String> tallaComboBox = new JComboBox<>(tallas);
         
         // Crear un JComboBox para la cantidad
+        Integer[] cantidades = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Opciones de cantidad
+        JComboBox<Integer> cantidadComboBox = new JComboBox<>(cantidades);
 
     }
 }
