21c59b09bc4029b03b6c3077213023b5369ca874
Naroa Rodriguez <naroa.r.a@opendeusto.es>
2025-01-11 23:31:05 +0100

    prueba

--- src/gui/Administrador.java
+++ src/gui/Administrador.java
@@ -46,8 +46,6 @@ package gui;
 	        listaProductos = cargarProductosDesdeCSV("productos.csv");
 	        actualizarTabla(listaProductos);
 	        
-	        //Prueba
-	        
 	        // Crear el botón para volver al menú
 	        JButton botonVolver = new JButton("Volver al Menú Administrador");
 	        
